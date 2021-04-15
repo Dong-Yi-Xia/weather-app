f1d63150bd7ce2eab52468a4702e6dc0711261cd
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-04-15 15:01:30 -0400

    add weather api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 // require('dotenv').config()
 import './App.css';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {dateBuilder2} from './components/date'
 
 const api = {
   key: process.env.REACT_APP_WEATHER,
-  base: 'api.openweathermap.org/data/2.5/'
+  base: 'https://api.openweathermap.org/data/2.5/'
 }
 
 function App() {
+
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+
+  // useEffect( async() => {
+  //   let response = await fetch(`${api.base}weather?q=${query}&units=imperial&appid=${api.key}`)
+  //   let data = await response.json()
+  //   setQuery('')
+  //   setWeather(data)
+  // }, [])
+
+  // const search = (evt) => {
+  //   if(evt.key === 'Enter'){
+  //     fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+  //       .then(res => res.json())
+  //       .then(result => {
+  //         setQuery('')
+  //         setWeather(result)
+  //         console.log(result)
+  //       })
+  //       .catch(err => {
+  //         console.log(err)
+  //       })
+  //   }
+  // }
+  
+
+  const search = async (evt) => {
+    if(evt.key === 'Enter'){
+      let response = await fetch(`${api.base}weather?q=${query}&units=imperial&appid=${api.key}`)
+      let data = await response.json()
+      setQuery('')
+      setWeather(data)
+      console.log(data)
+    }
+  }
 
   return (
     <div className="app ">
@@ -17,14 +54,25 @@ function App() {
                 <input 
                   type='text'
                   className='search-bar'
-                  placeholder='Search.....'
+                  placeholder='Search a city name...'
+                  onChange={e => setQuery(e.target.value)}
+                  value={query}
+                  onKeyPress={search}
                 />
             </div>
-
-            <div className='location-box'>
-                <div className='location'>New York City, US</div>
-                <div className='date'>{dateBuilder2()}</div>
-            </div>
+            {(typeof weather.main != 'undefined') ? (
+              <div>     
+                <div className='location-box'>
+                    <div className='location'>{weather.name}, {weather.sys.country} </div>
+                    <div className='date'>{dateBuilder2()}</div>
+                </div>
+    
+                <div className='weather-box'>
+                  <div className='temp'> {Math.round(weather.main.temp)}°F </div>
+                  <div className='weather'> {weather.weather[0].main} </div>
+                </div>
+              </div>
+            ) : ('City Not Found')}
 
         </main>
     </div>
